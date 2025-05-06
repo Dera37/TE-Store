a961f4e4a9a3e403a3f526d1b39a9c117c407b42
Dera37 <deraardanik8@gmail.com>
2025-05-06 08:07:44 +0700

    membuat t_kategori

--- admin/kategori.php
+++ admin/kategori.php
@@ -163,9 +163,8 @@
             <h1>kategori Produk</h1>
             <nav>
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                    <li class="breadcrumb-item">Tables</li>
-                    <li class="breadcrumb-item active">General</li>
+                    <li class="breadcrumb-item"><a href="index.html">Beranda</a></li>
+                    <li class="breadcrumb-item active">Kategori</li>
                 </ol>
             </nav>
         </div><!-- End Page Title -->
--- admin/t_kategori.php
+++ admin/t_kategori.php
@@ -86,13 +86,7 @@ if (isset($_POST['simpan'])) {
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <img src="assets/img/drnk.jpg" alt="Profile" class="rounded-circle">
 
-    <div class="search-bar">
-      <form class="search-form d-flex align-items-center" method="POST" action="#">
-        <input type="text" name="query" placeholder="Search" title="Enter search keyword">
-        <button type="submit" title="Search"><i class="bi bi-search"></i></button>
-      </form>
-    </div><!-- End Search Bar -->
-
+  
     <nav class="header-nav ms-auto">
       <ul class="d-flex align-items-center">
 
@@ -104,9 +98,6 @@ if (isset($_POST['simpan'])) {
 
         <li class="nav-item dropdown pe-3">
 
-          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-          </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
             <li class="dropdown-header">
