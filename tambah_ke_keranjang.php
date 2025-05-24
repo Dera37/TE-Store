ab568eedc1b8a8598298da78684c741bb9380f8b
Dera37 <deraardanik8@gmail.com>
2025-05-24 18:37:24 +0700

    merubah tambah ke keranjang

--- tambah_ke_keranjang.php
+++ tambah_ke_keranjang.php
@@ -1,14 +1,9 @@
 <?php
 session_start();
-
 include 'admin/koneksi.php';
 
-if (!isset($_SESSION['id_user'])) {
-    die ("<script>alert('Anda belum logon'); window.location.href='login.php';</script>");
-}
-
 $id_produk = $_POST['id_produk'];
-$id_user = $_SESSION['id_user'];
+$id_user = $_POST['id_user'];
 $jumlah = (int) $_POST['jumlah'];
 $harga = (int) $_POST['harga'];
 $redirect = isset($_POST['redirect_url']) ? $_POST['redirect_url'] : 'belanja.php';
@@ -21,8 +16,8 @@ if ($data && $data['stok'] >= $jumlah) {
     $total = $jumlah * $harga;
 
     // ambil di_pesanan terakhir
-    $getlast = mysqli_query($koneksi, "SELECT id_pesanan FROM tb_pesanan ORDER BY id_pesanan DESC LIMIT 1");
-    $lastData = mysqli_fetch_array($getlast);
+    $getLast = mysqli_query($koneksi, "SELECT id_pesanan FROM tb_pesanan ORDER BY id_pesanan DESC LIMIT 1");
+    $lastData = mysqli_fetch_assoc($getlast);
 
     if ($lasData) {
         $lastId = (int) substr($lastData['id_pesanan'], 1);
@@ -34,15 +29,20 @@ if ($data && $data['stok'] >= $jumlah) {
     $id_pesanan = 'M' . str_pad($newId, 3, '0', STR_PAD_LEFT);
 
     // Simpan ke tb_pesanan
-    $insert = mysqli_query($koneksi, "INSERT INTO tb_pesanan (id_pesanan, id_produk, qty, total, id_user) VALUES ('$id_pesanan', '$id_produk', '$jumlah', '$total', 'id_user') ");
+    $insert = mysqli_query($koneksi, "INSERT INTO tb_pesanan 
+    (id_pesanan, id_produk, qty, total, id_user) VALUES ('$id_pesanan', '$id_produk', '$jumlah', '$total', '$id_user') ");
     if ($insert) {
         // Kurangi stok
-        mysqli_query($koneksi, "UPDATE tb_produk SET stok = stok - $jumlah WHERE id_produk = 'id_produk'");
-        echo "<script>alert('Produk berhasil ditambahkan'); window.location.href='cart.php';</script>";
+        mysqli_query($koneksi, "UPDATE tb_produk SET stok = stok - $jumlah 
+        WHERE id_produk = 'id_produk'");
+        echo "<script>alert('Produk berhasil ditambahkan ke pesanan.'); 
+        window.location.href='cart.php';</script>";
     } else {
-        echo "<script>alert('Gagal menyimpan pesan'); window.location.href='$redirect';</script>";
+        echo "<script>alert('Gagal menyimpan pesanan.'); 
+        window.location.href='$redirect';</script>";
     }
 } else {
-    echo "<script>alert('Stok tidak mencukupi'); window.location.href='$redirect';</script>";
+    echo "<script>alert('Stok tidak mencukupi.'); 
+    window.location.href='$redirect';</script>";
 }
 ?>
