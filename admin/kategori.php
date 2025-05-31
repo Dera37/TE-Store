25f059d8d31283fe4434b3a0953b9b1236e37739
Dera37 <deraardanik8@gmail.com>
2025-05-31 21:13:50 +0700

    merubah kategori

--- admin/kategori.php
+++ admin/kategori.php
@@ -178,7 +178,7 @@ if (!isset($_SESSION["status"]) || $_SESSION["status"] !== "admin") {
     <main id="main" class="main">
 
         <div class="pagetitle">
-            <h1>kategori Produk</h1>
+            <h1>Kategori Produk</h1>
             <nav>
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="index.php">Beranda</a></li>
