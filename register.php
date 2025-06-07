3d344b0933663aa0b9f7d59a6c712ac3daf372cb
Dera37 <deraardanik8@gmail.com>
2025-06-07 22:59:16 +0700

    mengganti tittle di register

--- register.php
+++ register.php
@@ -19,7 +19,7 @@ $data = mysqli_fetch_array($sql);
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Register - Te store</title>
+    <title>Register - TE-Store</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
