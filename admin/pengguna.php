c78163992c07462bbfd8ba7ac536a0407ffc9e61
Dera37 <deraardanik8@gmail.com>
2025-06-10 19:31:49 +0700

    merubah footer di file pengguna

--- admin/pengguna.php
+++ admin/pengguna.php
@@ -268,7 +268,7 @@ if (!isset($_SESSION["status"]) || $_SESSION["status"] !== "admin") {
             &copy; Copyright <strong><span>TE-Store</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
-            Designed by <a href="https://instagram.com/dea.salsa.503/" target="_blank">Dera ardanik</a>
+            Designed by <a href="https://instagram.com/dea.salsa.503/" target="_blank">Dera Ardanik</a>
         </div>
     </footer><!-- End Footer -->
 
